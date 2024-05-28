baffb37e2bcec4b4a3b463a07622ceb522d35f9b
Mike T <82486161+mtesema@users.noreply.github.com>
2024-05-27 23:30:18 -0400

    stripe checkout complete. working on firebase db integration

--- Amazon-Clone-React-2024/backend/Webhook.jsx
+++ Amazon-Clone-React-2024/backend/Webhook.jsx
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const admin = require("firebase-admin");
-const serviceAccount = require("./serviceAccountKey.json");
+const serviceAccount = require("../serviceAccountKey.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
