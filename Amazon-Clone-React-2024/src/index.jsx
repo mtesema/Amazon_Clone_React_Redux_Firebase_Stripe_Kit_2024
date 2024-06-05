e57fa79589ba340fb88bdb9810a8f11074dfa444
Mike T <82486161+mtesema@users.noreply.github.com>
2024-06-05 15:50:54 -0400

    Add essential files for deployment

---
+++ Amazon-Clone-React-2024/src/index.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { StateProvider } from "./Utility/StateProvider.jsx";
+import reducer, { initialState } from "./Utility/Reducer.jsx";
+import "./index.css";
+
+// Load environment variables
+// const { VITE_STRIPE_PUBLIC_KEY } = import.meta.env;
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <StateProvider initialState={initialState} reducer={reducer}>
+      <App />
+    </StateProvider>
+  </React.StrictMode>
+);
+
+// console.log("Stripe Public Key:", VITE_STRIPE_PUBLIC_KEY);
